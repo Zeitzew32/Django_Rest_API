20941e1fb9af633a9372211509a1c07948ed2d07
Jacob <zeitzew32@gmail.com>
2020-07-14 21:18:00 -0700

    First commit

---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
